9cfd4020b14849a3d7f866fde3ced418133cf349
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-01 14:59:29 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class dog
+def initialize (name)
+    @name=name
+  end
+
+def initialize (breed)
+  @breed=breed
+end
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class person
+  def initialize (name)
+    @name=name
+  end
+  
+  def initialize (job)
+    @job=job
+  end
