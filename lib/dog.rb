1430d8d0cb29143a6f4c2d8c2b4f3069ed33e587
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-14 17:40:01 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,19 @@
 class dog
-def initialize (name)
+  
+def name
     @name=name
   end
+  
+  def name =(name)
+    @name= name
+  end
 
-def initialize (breed)
-  @breed=breed
+def breed
+  @breed
+end
+
+def breed =(breed)
+  @breed = breed
+ end
+
 end
